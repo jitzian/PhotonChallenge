c2ceeb368f13415359ec09d36854c505fc140b79
Ricardo Viera <rvscript@gmail.com>
2018-01-29 12:45:06 -0500

    updated grid and test

--- app/src/main/java/com/example/rv193/photonchallenge/worker/Grid.java
+++ app/src/main/java/com/example/rv193/photonchallenge/worker/Grid.java
@@ -1,7 +1,7 @@
 package com.example.rv193.photonchallenge.worker;
 
 import android.support.annotation.NonNull;
-import android.util.Log;
+import android.support.annotation.VisibleForTesting;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -10,75 +10,78 @@ import java.util.Set;
 
 public class Grid {
 
-        private final int[][] values;
+    private final int[][] values;
 
-        public Grid(int[][] values)
-        {
-            //Log.d("Grid", "The length is " + values.length);
-            if ((values.length < 1) || (values.length > 10))
-                throw new IllegalArgumentException("Between one and ten rows of values are expected");
-            if ((values[0].length < 1) || (values[0].length > 100)) {
-                throw new IllegalArgumentException("Between five and one hundred columns of values are expected");
-            }
-
-            this.values = values;
+    public Grid(int[][] values)
+    {
+        //Log.d("Grid", "The length is " + values.length);
+        if ((values.length < 1) || (values.length > 10))
+            throw new IllegalArgumentException("Between one and ten rows of values are expected");
+        if ((values[0].length < 1) || (values[0].length > 100)) {
+            throw new IllegalArgumentException("Between five and one hundred columns of values are expected");
         }
 
-        public int getValueForRowAndColumn(int row, int column) {
-            return this.values[(row - 1)][(column - 1)];
-        }
-
-        public int getRowCount() {
-            return this.values.length;
-        }
-
-        public int getColumnCount() {
-            return this.values[0].length;
-        }
-
-        @NonNull
-        public List<Integer> getRowsAdjacentTo(int rowNumber) {
-            final Set adjacentRows = new HashSet();
-
-            if (isValidRowNumber(rowNumber)) {
-                adjacentRows.add(Integer.valueOf(rowNumber));
-                adjacentRows.add(Integer.valueOf(getRowAbove(rowNumber)));
-                adjacentRows.add(Integer.valueOf(getRowBelow(rowNumber)));
-            }
-
-            return new ArrayList(adjacentRows);
-        }
-
-        @NonNull
-        public String asDelimitedString(String delimiter) {
-            final StringBuilder builder = new StringBuilder();
-
-            for (int row = 0; row < this.values.length; row++) {
-                for (int column = 0; column < this.values[row].length; column++) {
-                    builder.append(this.values[row][column]);
-                    if (column < this.values[row].length - 1) {
-                        builder.append(delimiter);
-                    }
-                }
-                if (row < this.values.length - 1) {
-                    builder.append("\n");
-                }
-            }
-
-            return builder.toString();
-        }
-
-        private boolean isValidRowNumber(int rowNumber) {
-            return (rowNumber > 0) && (rowNumber <= this.values.length);
-        }
-
-        private int getRowAbove(int rowNumber) {
-            int potentialRowNumber = rowNumber - 1;
-            return potentialRowNumber < 1 ? this.values.length : potentialRowNumber;
-        }
-
-        private int getRowBelow(int rowNumber) {
-            int potentialRowNumber = rowNumber + 1;
-            return potentialRowNumber > this.values.length ? 1 : potentialRowNumber;
-        }
+        this.values = values;
     }
+
+    public int getValueForRowAndColumn(int row, int column) {
+        return this.values[(row - 1)][(column - 1)];
+    }
+
+    public int getRowCount() {
+        return this.values.length;
+    }
+
+    public int getColumnCount() {
+        return this.values[0].length;
+    }
+
+    @NonNull
+    public List<Integer> getRowsAdjacentTo(int rowNumber) {
+        final Set adjacentRows = new HashSet();
+
+        if (isValidRowNumber(rowNumber)) {
+            adjacentRows.add(Integer.valueOf(rowNumber));
+            adjacentRows.add(Integer.valueOf(getRowAbove(rowNumber)));
+            adjacentRows.add(Integer.valueOf(getRowBelow(rowNumber)));
+        }
+
+        return new ArrayList(adjacentRows);
+    }
+
+    @NonNull
+    public String asDelimitedString(String delimiter) {
+        final StringBuilder builder = new StringBuilder();
+
+        for (int row = 0; row < this.values.length; row++) {
+            for (int column = 0; column < this.values[row].length; column++) {
+                builder.append(this.values[row][column]);
+                if (column < this.values[row].length - 1) {
+                    builder.append(delimiter);
+                }
+            }
+            if (row < this.values.length - 1) {
+                builder.append("\n");
+            }
+        }
+
+        return builder.toString();
+    }
+
+    @VisibleForTesting
+    boolean isValidRowNumber(int rowNumber) {
+        return (rowNumber > 0) && (rowNumber <= this.values.length);
+    }
+
+    @VisibleForTesting
+    int getRowAbove(int rowNumber) {
+        int potentialRowNumber = rowNumber - 1;
+        return potentialRowNumber < 1 ? this.values.length : potentialRowNumber;
+    }
+
+    @VisibleForTesting
+    int getRowBelow(int rowNumber) {
+        int potentialRowNumber = rowNumber + 1;
+        return potentialRowNumber > this.values.length ? 1 : potentialRowNumber;
+    }
+}
--- app/src/test/java/com/example/rv193/photonchallenge/worker/GridTest.java
+++ app/src/test/java/com/example/rv193/photonchallenge/worker/GridTest.java
@@ -1,40 +1,47 @@
 package com.example.rv193.photonchallenge.worker;
 
-import org.junit.Before;
+        import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.internal.matchers.NotNull;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
-import static org.junit.Assert.*;
-
-/**
- * Created by rv193 on 1/25/2018.
- */
 public class GridTest {
 
-    private static final int[][]TEMP = new int[][]{{5,8,5,3,5}};
+    private static final int[][] TEMP = new int[][]{{5, 8, 5, 3, 5}};
 
-    @Mock
-    Grid grid;
-
-    private static final String CORRECT = "5    8   5   3   5";
-    private static final String DELIMITER = "\t";
+    private Grid grid;
 
     @Before
-    public void setupGridTest(){
-        MockitoAnnotations.initMocks(this);
-
+    public void setupGridTest() {
         grid = new Grid(TEMP);
     }
 
     @Test
+    public void checkValidRowNumber() {
+        assertTrue(grid.isValidRowNumber(1));
+    }
+
+    @Test
+    public void falseOnZeroRow() {
+        assertFalse(grid.isValidRowNumber(0));
+    }
+
+    @Test
     public void getValueForRowAndColumn() throws Exception {
-        assertEquals(5, TEMP[(TEMP.length-1)][(TEMP[0].length-1)]);
+        assertEquals(5, TEMP[(TEMP.length - 1)][(TEMP[0].length - 1)]);
+    }
+
+    @Test
+    public void getRowAboveIsLength() {
+        assertEquals(TEMP.length, grid.getRowAbove(1));
+    }
+
+    @Test
+    public void getRowBelowIsLength() {
+        assertEquals(TEMP.length, grid.getRowBelow(5));
     }
 
     @Test
@@ -53,7 +60,7 @@ public class GridTest {
     }
 
     @Test
-    public void getRowsAdjacentToNull()throws Exception{
+    public void getRowsAdjacentToNull() throws Exception {
         assertNotNull(grid.getRowsAdjacentTo(TEMP[0][0]));
     }
 }
